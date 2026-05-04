6f776e034fa10b74f8873d75a8b76ba9587bdf25
MohammadAloreidi <mohammadorade2@gmail.com>
2026-05-04 17:09:42 +0300

    Mohammed: Final Day 1 - Supabase initialization success

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:just_bus_tracker/screens/student/login_screen.dart'; // تأكد من صحة المسار
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:just_bus_tracker/screens/student/login_screen.dart'; 
 
-void main() {
-  runApp(const MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: LoginScreen(), // عرض شاشتك الجديدة
-  ));
+void main() async {
+  // 1. ضمان تهيئة جميع خدمات فلاتر قبل تشغيل التطبيق
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // 2. ربط التطبيق بمشروع Supabase (البيانات التي أرسلها إبراهيم الكردي)
+  await Supabase.initialize(
+    url: 'https://regtuxnoulgwfpyegohc.supabase.co',
+    anonKey: 'sb_publishable_yl_OWdKFQIhmuLIOba2V9A_FtoPMYev',
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Just Bus Tracker',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        useMaterial3: true,
+      ),
+      home: const LoginScreen(),
+    );
+  }
 }
