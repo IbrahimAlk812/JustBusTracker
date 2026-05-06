9ba223436756907bed7ade134a7ca1e99b70d250
ibrahimkordi <kordim365@gmail.com>
2026-05-07 02:29:51 +0300

    kordi: Added emergency report button to driver screen

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:just_bus_tracker/screens/student/student_home_screen.dart';
 import 'package:just_bus_tracker/screens/supervisor/supervisor_dashboard.dart';
 import 'package:just_bus_tracker/screens/driver/driver_screen.dart';
 import 'package:just_bus_tracker/screens/supervisor_bus_table.dart';
+import 'package:just_bus_tracker/screens/driver/driver_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -26,7 +27,8 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Just Bus Tracker',
       theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.blue),
-      home: const SupervisorBusTable(),
+      home: const DriverScreen(),
+      
     );
   }
 }
